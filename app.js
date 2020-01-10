43fefa96e277df019bb6bd310b35f29d028e0fb8
Gabby Sanghera <gabby.sanghera@gmail.com>
2020-01-10 16:14:22 +0000

    first commit using sails, johnny five installed, links to arduino board

---
+++ app.js
@@ -0,0 +1,80 @@
+/**
+ * app.js
+ *
+ * Use `app.js` to run your app without `sails lift`.
+ * To start the server, run: `node app.js`.
+ *
+ * This is handy in situations where the sails CLI is not relevant or useful,
+ * such as when you deploy to a server, or a PaaS like Heroku.
+ *
+ * For example:
+ *   => `node app.js`
+ *   => `npm start`
+ *   => `forever start app.js`
+ *   => `node debug app.js`
+ *
+ * The same command-line arguments and env vars are supported, e.g.:
+ * `NODE_ENV=production node app.js --port=80 --verbose`
+ *
+ * For more information see:
+ *   https://sailsjs.com/anatomy/app.js
+ */
+
+
+// Ensure we're in the project directory, so cwd-relative paths work as expected
+// no matter where we actually lift from.
+// > Note: This is not required in order to lift, but it is a convenient default.
+process.chdir(__dirname);
+
+
+
+// Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
+var sails;
+var rc;
+try {
+  sails = require('sails');
+  rc = require('sails/accessible/rc');
+} catch (err) {
+  console.error('Encountered an error when attempting to require(\'sails\'):');
+  console.error(err.stack);
+  console.error('--');
+  console.error('To run an app using `node app.js`, you need to have Sails installed');
+  console.error('locally (`./node_modules/sails`).  To do that, just make sure you\'re');
+  console.error('in the same directory as your app and run `npm install`.');
+  console.error();
+  console.error('If Sails is installed globally (i.e. `npm install -g sails`) you can');
+  console.error('also run this app with `sails lift`.  Running with `sails lift` will');
+  console.error('not run this file (`app.js`), but it will do exactly the same thing.');
+  console.error('(It even uses your app directory\'s local Sails install, if possible.)');
+  return;
+}//-•
+
+
+// Start server
+sails.lift(rc('sails'));
+
+var five = require('johnny-five');
+
+// Make variables for objects we'll be using
+var board, led, pushButton;
+
+// Make a new Board Instance
+board = new five.Board();
+
+// When the board's connected, turn on the LED connected to pin 9
+board.on('ready', function() {
+  console.log('[johnny-five] Board is ready.');
+
+  // Make a new Led object and connect it to pin 9
+  led = new five.Led(9);
+
+  // Switch it on!
+  led.on();
+
+  // When the board is closing, stop any LED animations and turn it off
+  this.on('exit', function() {
+    led.stop().off();
+    console.log('[johnny-five] Bye Bye.');
+  });
+
+});
